7762b8b1b54c6664dff26cfcd6b1961cfa0dfa32
Bamidele Ademokoya <bamyonline@gmail.com>
2023-09-15 18:16:32 +0100

    Completed header and home components

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,25 @@
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a 
+        href="#" 
+        className="home__social-icon" target="_blank">
+        <i class="uil uil-linkedin-alt"></i>
+        </a>
+
+        <a 
+        href="#" 
+        className="home__social-icon" target="_blank">
+        <i class="uil uil-github-alt"></i>
+        </a>
+
+        <a 
+        href="#" 
+        className="home__social-icon" target="_blank">
+        <i class="uil uil-twitter-alt"></i>
+        </a>
+
+    </div>
+  )
+}
+export default Social
