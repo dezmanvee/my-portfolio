fa94034a0c5e6f04324eee5fb1ad4e1b0289ceb0
Bamidele Ademokoya <bamyonline@gmail.com>
2023-10-24 02:41:38 +0100

    Add data to project

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,40 +1,49 @@
 import work1 from "../../assets/1.png";
 import work2 from "../../assets/2.png";
 import work3 from "../../assets/3.png";
-import work4 from "../../assets/4.png"
+import work4 from "../../assets/4.png";
+import work5 from "../../assets/5.png";
 
 
 
  export const ProjectsData = [
-    {
-      id: 1,
-      image: work1,
-      title: "ToDo App",
-      description: "A robust ToDo app that enables users to efficiently manage tasks with full CRUD functionality.",
-      url: "https://todo-dynamic-app.netlify.app/"
-    },
-    {
-      id: 2,
-      image: work2, 
-      title: "ComeNcarT",
-      description: "An e-commerce application with product listings, and shopping cart functionality.",
-      url: "https://comencart.netlify.app/"
-    },
-    {
-      id: 4,
-      image: work4, 
-      title: "Shoeplug",
-      description: "A cloned Nike landing page, expertly replicating the brand's signature design and user experience. ",
-      url: "https://shoeplug.netlify.app/"
-    },
-    {
-      id: 3,
-      image: work3, 
-      title: "AdminPro",
-      description: "A static dashboard that provides snapshot insights for informed decision-making within dataset.",
-      url: "https://adminpro-dashboard.netlify.app/"
-    },
-  ];
+  {
+    id: 5,
+    image: work5, 
+    title: "Modern Bank App",
+    description: "A sleek and modern landing page for a cutting-edge digital banking experience.",
+    url: "https:oobank.netlify.app/"
+  },
+  {
+    id: 1,
+    image: work1,
+    title: "ToDo App",
+    description: "A robust ToDo app that enables users to efficiently manage tasks with full CRUD functionality.",
+    url: "https://todo-dynamic-app.netlify.app/"
+  },
+  {
+    id: 2,
+    image: work2, 
+    title: "ComeNcarT",
+    description: "An e-commerce application with product listings, and shopping cart functionality.",
+    url: "https://comencart.netlify.app/"
+  },
+  {
+    id: 4,
+    image: work4, 
+    title: "Shoeplug",
+    description: "A cloned Nike landing page, expertly replicating the brand's signature design and user experience.",
+    url: "https://shoeplug.netlify.app/"
+  },
+  {
+    id: 3,
+    image: work3, 
+    title: "AdminPro",
+    description: "A static dashboard that provides snapshot insights for informed decision-making within dataset.",
+    url: "https://adminpro-dashboard.netlify.app/"
+  },
+];
+
   
 
   
